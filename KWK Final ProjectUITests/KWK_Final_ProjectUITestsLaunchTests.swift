e7cb2d63c7acd885518c2f696d29e5520cbd82fa
mk <itgirlarchive@icloud.com>
2024-07-12 13:02:17 -0700

    Initial Commit

---
+++ KWK Final ProjectUITests/KWK_Final_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KWK_Final_ProjectUITestsLaunchTests.swift
+//  KWK Final ProjectUITests
+//
+//  Created by star girl on 7/12/24.
+//
+
+import XCTest
+
+final class KWK_Final_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
