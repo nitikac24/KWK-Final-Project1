fdb62aaedea1a24e92f732be90a391983d429cb3
Anika <prakashanika5@gmail.com>
2024-07-17 12:57:22 -0700

    Create education page, add a slideshow of water pollution images on page, set background color

---
+++ KWK Final Project/educationPage.swift
@@ -0,0 +1,18 @@
+//
+//  educationPage.swift
+//  KWK Final Project
+//
+//  Created by Scholar on 7/17/24.
+//
+
+import SwiftUI
+
+struct educationPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    educationPage()
+}
